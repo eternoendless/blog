cada6cb4f15a1bb0af23ffec9a0501d78153e2c0
Pablo Borowicz <eternoendless@gmail.com>
2020-09-15 16:37:11 +0200

    Fix hugo version to work with sass

--- bin/installHugo.sh
+++ bin/installHugo.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 export HUGO_VERSION="0.74.3"
-wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
-tar -xzf hugo_${HUGO_VERSION}_Linux-64bit.tar.gz hugo && rm hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
+wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+tar -xzf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz hugo && rm hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
