49c6c53277d8bc8d70811931afab5cca05125600
Pablo Borowicz <eternoendless@gmail.com>
2022-04-20 17:32:56 +0200

    Update hugo to 0.97

--- bin/installHugo.sh
+++ bin/installHugo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-export HUGO_VERSION="0.74.3"
+export HUGO_VERSION="0.97.0"
 wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
 tar -xzf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz hugo && rm hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
